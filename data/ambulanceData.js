6448101906a09a4b0046dbd06791a30110813f3a
Moin <ourmoin@gmail.com>
2023-06-20 18:00:34 +0000

    new page added

--- data/ambulanceData.js
+++ data/ambulanceData.js
@@ -7,7 +7,7 @@ const userData = [
     wordNo: "অন্যান্য",
     location: "আদর্শ নগর, কুতুবপুর",
     detail: "কুতুবপুরের সবচেয়ে নিকটতম এম্বুল্যান্স সার্ভিস। এসি-নন, এসি/ অক্সিজেন সহ দিন রাত ২৪ ঘন্টা সমগ্র বাংলাদেশে এম্বুল্যান্স সার্ভিস দেওয়া হয়।",
-    postLink: "https://www.facebook.com/AreaPhonebook/",
+    postLink: "./ambulance-details.html",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
@@ -18,7 +18,7 @@ const userData = [
     wordNo: "৬",
     location: "সমগ্র পাগলা",
     detail: "২৪ ঘন্টা পাগলা এলাকার সবচেয়ে নিকটবর্তী এ্যাম্বুলেন্স সার্ভিস। সকল সেবা ঢাকা মেডিকেল ও সমগ্র বাংলাদেশে উন্মুক্ত।",
-    postLink: "https://www.facebook.com/AreaPhonebook/",
+    postLink: "data/ambulanceData.js",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
@@ -29,7 +29,7 @@ const userData = [
     wordNo: "সমগ্র পাগলা",
     location: "সমগ্র পাগলা",
     detail: "সারা বাংলাদেশে এম্বুলেন্স সার্ভিস দিয়ে থাকে।<br> ২৪ ঘণ্টায় নিয়োজিত ও জরুরি প্রয়োজনে অক্সিজেনের ব্যবস্থা, এসি/ নন এসি, লাশবাহি ফ্রিজিং, আইসিও ও সিসিও এম্বুলেন্স পাওয়া যায়।",
-    postLink: "https://www.facebook.com/AreaPhonebook/",
+    postLink: "data/ambulanceData.js",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
   
---
+++ data/ambulanceDetailsData.js
@@ -0,0 +1,14 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/ambulance/ambulance-1.jpg",
+    name: "বাফিন এম্বুল্যান্স সার্ভিস",
+    number: "01865806372",
+    wordNo: "অন্যান্য",
+    location: "আদর্শ নগর, কুতুবপুর",
+    detail: "কুতুবপুরের সবচেয়ে নিকটতম এম্বুল্যান্স সার্ভিস। এসি-নন, এসি/ অক্সিজেন সহ দিন রাত ২৪ ঘন্টা সমগ্র বাংলাদেশে এম্বুল্যান্স সার্ভিস দেওয়া হয়। আমার সোনার বাংলা আমি তমায় ভালোবাসি চিরো দিন তোমার আকাস তোমার বাতাস আম্র প্রানে ও মা আমার প্রানে বাজায় বাসি আম্র সোনার বাংলা আমি তোমায়া ভালবাশি",
+    postLink: "https://www.facebook.com/AreaPhonebook/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+  
+];
